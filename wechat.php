b23626215da8bb4ddee0d589d52075a70286efb5
liu <827947510@qq.com>
2016-05-21 18:13:13 +0800

    更新获取ticket

--- wechat.php
+++ wechat.php
@@ -46,4 +46,79 @@ class wechat{
 		$url="https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
 		return json_decode(file_get_contents($url));
 	}
+
+
+	/**
+	 * 签名
+	 * @return [type] [description]
+	 */
+	public function sign($url,$timestamp,$noncestr)
+	{
+		// $ticket=$this->jsapi_ticket($access_token);
+		$access_token=$this->gettoken();
+		$ticket=$this->getticket($access_token);
+
+		$params['jsapi_ticket']=$ticket;
+		$params['url']=$url;
+		$params['timestamp']=$timestamp;
+		$params['noncestr']=$noncestr;
+		ksort($params);
+
+		$str='';
+		foreach ($params as $key => $value)
+			$str.="&$key=$value";
+		$str=substr($str, 1);
+		return sha1($str);
+	}
+
+	/**
+	 * 获取ticket,缓存在SESSION中
+	 * @param  [type] $access_token [description]
+	 * @return [type]               [description]
+	 */
+	public function getticket($access_token)
+	{
+		session_start();
+		if(!isset($_SESSTION['ticket']) || $_SESSTION['ticket']['expire']<time())
+		{
+			$ticket=$this->jsapi_ticket($access_token);
+			$_SESSTION['ticket']=[
+				'val'=>$ticket->ticket,
+				'expire'=>time()+$ticket->expire
+			];
+			return $ticket->ticket;
+		}else
+			return $_SESSTION['ticket']['val'];
+	}
+
+	/**
+	 * 根据access_token获得jsapi_ticket
+	 * @param  [type] $access_token [description]
+	 * @return [type]               [description]
+	 */
+	public function jsapi_ticket($access_token)
+	{
+		$url="https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=$access_token&type=jsapi";
+		return json_decode(file_get_contents($url));
+	}
+
+	/**
+	 * 获得accesstoken,缓存在SESSION中
+	 * @return [type] [description]
+	 */
+	public function gettoken()
+	{
+		session_start();
+		if(!isset($_SESSTION['access_token']) || $_SESSTION['access_token']['expire']<time())
+		{
+			$url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->appid&secret=$this->secret";
+			$access_token=json_decode(file_get_contents($url));
+			$_SESSTION['access_token']=[
+				'val'=>$access_token->access_token,
+				'expire'=>time()+$access_token->expire
+			];
+			return $access_token->access_token;
+		}else
+			return $_SESSTION['access_token']['val'];
+	}
 }
